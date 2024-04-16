6d65c3abc0c8c91f61f27bbacea942c90c4b6d1f
Salvador Jimenez <salvad.j.f@gmail.com>
2024-04-16 22:25:02 +0200

    Creacion de Las migraciones y modelos inciales

---
+++ database/factories/TexturaFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Textura>
+ */
+class TexturaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
