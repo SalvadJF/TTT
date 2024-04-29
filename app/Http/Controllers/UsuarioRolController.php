ddddacd2d0471becc4582c89e4e02834dd5e9734
SalvadJF <salvad.j.f@gmail.com>
2024-04-29 13:26:48 +0200

    Creacion de las migraciones intermedias

---
+++ app/Http/Controllers/UsuarioRolController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario_Rol;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUsuario_RolRequest;
+use App\Http\Requests\UpdateUsuario_RolRequest;
+
+class UsuarioRolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUsuario_RolRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Usuario_Rol $usuario_Rol)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario_Rol $usuario_Rol)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUsuario_RolRequest $request, Usuario_Rol $usuario_Rol)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Usuario_Rol $usuario_Rol)
+    {
+        //
+    }
+}
