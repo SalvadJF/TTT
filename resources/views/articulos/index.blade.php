5d39c4792a09a0c2ad929e5b3470f3236f251047
SalvadJF <salvad.j.f@gmail.com>
2024-05-09 13:09:41 +0200

    Pruebas para modificar el fondo confirmadas

--- resources/views/articulos/index.blade.php
+++ resources/views/articulos/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="bg-light-gray">
         {{-- El Breadcrum para saber donde estamos --}}
-        <nav class="flex m-10" aria-label="Breadcrumb">
+        <nav class="flex pl-10 pt-10" aria-label="Breadcrumb">
             <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
               <li class="inline-flex items-center">
                 <a href="{{ route('home') }}" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
