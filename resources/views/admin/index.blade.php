f4ba8c2046ffc87b236ec029ccd753bcd6f788ba
SalvadJF <salvad.j.f@gmail.com>
2024-05-20 10:59:42 +0200

    Cambio de los plantillas por defecto de Laravel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -29,6 +29,15 @@
             <div class="font-koulen">{{$admin->name}}</div>
             <div class="font-lato text-sm text-gray-100 dark:text-gray-400">Bienvenido Administrador</div>
         </div>
+        <div>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a href="{{ route('logout') }}"
+                    class="font-lato block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    onclick="event.preventDefault();
+  this.closest('form').submit();">
+                    {{ __('Salir') }}</a>
+        </div>
     </div>
 
 
