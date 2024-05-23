e18e3e59d8029070d0672f8babb0d10dee83c6bb
SalvadJF <salvad.j.f@gmail.com>
2024-05-23 18:16:24 +0200

    Buscador en mi perfil

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,22 +1,12 @@
-export default function Pagination ({ postsPerPage, totalPosts, paginate }) {
-  const pageNumbers = [];
-
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
-  return (
-    <nav>
-      <ul className='pagination'>
-        {pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link'>
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+export default function Pagination({ currentPage, totalPages, onPageChange }) {
+    return (
+        <div className="flex justify-center mt-4">
+            {Array.from({ length: totalPages }).map((_, index) => (
+                <button key={index} onClick={() => onPageChange(index + 1)} className={`px-3 py-1 mx-1 rounded-md ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>
+                    {index + 1}
+                </button>
+            ))}
+        </div>
+    );
+}
 
