e41678a50854543eb799f1ebd4d47180393b95a0
SalvadJF <salvad.j.f@gmail.com>
2024-06-04 20:35:43 +0200

    problema de estilos

--- resources/js/Components/Acordeon.jsx
+++ resources/js/Components/Acordeon.jsx
@@ -4,7 +4,7 @@ function Acordeon({ title, isOpen, onToggle, children }) {
             <h2 id={`accordion-color-heading-${title}`}>
                 <button
                     type="button"
-                    className={`text-center flex items-center justify-between w-full p-5 font-medium rtl:text-right text-white border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-800 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-700 dark:hover:bg-gray-800 gap-3 ${
+                    className={`text-center flex items-center justify-between w-full p-5 font-koulen rtl:text-right text-white border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-800 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-700 dark:hover:bg-gray-800 gap-3 ${
                         isOpen ? "bg-blue-900 dark:bg-blue-900" : ""
                     }`}
                     data-accordion-target={`#accordion-color-body-${title}`}
